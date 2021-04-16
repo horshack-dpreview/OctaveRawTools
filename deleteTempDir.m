987f73916cd66f442075334a194fc818d29cabb0
Horshack <horshack@live.com>
2021-04-16 12:03:47 -0700

    deleteTempDir(): Supress warnings when there are no files to delete

--- deleteTempDir.m
+++ deleteTempDir.m
@@ -14,7 +14,9 @@
 function deleteTempDir(tempDirPath)
 
   fprintf('Deleting temporary files and folder at "%s"\n', tempDirPath);
+  warning ('off'); % suppress warning from delete() for case when there are no files to delete
   delete(fullfile(tempDirPath, '*'));
+  warning ('on');
   rmdir(tempDirPath);
 
 end
